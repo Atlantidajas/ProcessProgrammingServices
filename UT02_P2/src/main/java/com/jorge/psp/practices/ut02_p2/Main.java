77cd7becfd947e238c7b40c336f17f530c83c269
Jorge H <jorge.hlt@hotmail.com>
2020-01-24 21:20:22 +0000

    Add MVC practice UT02_P2

---
+++ UT02_P2/src/main/java/com/jorge/psp/practices/ut02_p2/Main.java
@@ -0,0 +1,14 @@
+/*
+@ Author Jorge Herrán.
+@ Version 1.0
+@ Since 24/01/2020
+*/
+
+package com.jorge.psp.practices.ut02_p2;
+
+public class Main {
+
+    public static void main( String[] args ) {
+
+    }
+}
