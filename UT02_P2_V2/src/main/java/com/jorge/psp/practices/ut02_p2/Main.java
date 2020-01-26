3f0c40000b229466bc38ec2f50fa1d267c9abbe4
Jorge H <jorge.hlt@hotmail.com>
2020-01-26 19:27:13 +0000

    Funcionando version 1.0

---
+++ UT02_P2_V2/src/main/java/com/jorge/psp/practices/ut02_p2/Main.java
@@ -0,0 +1,26 @@
+/*
+@ Author Jorge Herrán.
+@ Version 2.0
+@ Since 26/01/2020
+*/
+
+package com.jorge.psp.practices.ut02_p2;
+
+import com.jorge.psp.practices.ut02_p2.models.Players;
+import com.jorge.psp.practices.ut02_p2.utils.CreatedWord;
+
+public class Main {
+
+    public static void main( String[] args ) {
+
+        CreatedWord createdWord = new CreatedWord( "Prueba" );
+
+        Players players1 = new Players( 0, createdWord.getLowerCaseWord()  );
+        Players players2 = new Players( 1, createdWord.getLowerCaseWord()  );
+        Players players3 = new Players( 2, createdWord.getLowerCaseWord()  );
+
+        players1.start();
+        players2.start();
+        players3.start();
+    }
+}
---
+++ UT02_P2_V2/src/main/java/com/jorge/psp/practices/ut02_p2/utils/GenerateRandomAlphabet.java
@@ -0,0 +1,25 @@
+/*
+@ Author Jorge Herrán.
+@ Version 2.0
+@ Since 26/01/2020
+*/
+
+package com.jorge.psp.practices.ut02_p2.utils;
+
+public class GenerateRandomAlphabet{
+
+    private int numRandon;
+    private boolean free = true;
+
+    private static final String[] ALPHABET = {
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+            "k", "l", "m","N", "ñ", "o","p","q","r","s","t","u","v","w", "x","y","z"};
+
+    public GenerateRandomAlphabet() {
+        this.numRandon = ( int ) Math.round( Math.random() * 26 );
+    }
+
+    public String getRandonAlphabet() {
+        return ALPHABET[this.numRandon];
+    }
+}
